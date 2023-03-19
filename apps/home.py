12ad6f1deba4c8037c22a95a7ec8f3e359b3af40
andreas-masaoutis <41254497+andreas-masaoutis@users.noreply.github.com>
2023-03-19 20:50:37 +0200

    Added new text file for Home page

--- apps/home.py
+++ apps/home.py
@@ -3,53 +3,26 @@ The home/main page
 """
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+
 
 # needed only if running this as a single page app
 # external_stylesheets = [dbc.themes.LUX]
 
 # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+with open("texts/home.md", encoding="utf-8") as md_file:
+    md_content = md_file.read()
+
+
 # change to app.layout if running as single page app instead
 layout = html.Div(
-    [
+    [   
         dbc.Container(
             [
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            html.H1(
-                                "The relation between the favourite - longshot bias\
-            and the bookmaker's overround",
-                                className="text-center",
-                            ),
-                            className="mb-5 mt-5",
-                        )
-                    ]
-                ),
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            html.H5(
-                                children="The favourite - longshot bias is well \
-            studied in the economics literature that analyses that beting market."
-                            ),
-                            className="mb-4",
-                        )
-                    ]
-                ),
-                dbc.Row(
-                    [
-                        dbc.Col(
-                            html.H5(
-                                children="The first page gives an overview of the literature. \
-            The second presents the dataset we will use. The third and fourth pages analyse \
-            the favourite - longshot bias and the overround respectively, by season, bookmaker \
-            and division. Finally we see how the overround affects the bias"
-                            ),
-                            className="mb-5",
-                        )
-                    ]
-                ),
+                html.Div([ dcc.Markdown(f"""{md_content}""")],
+                         style={"text-align": "justify"}),
+                            
                 dbc.Row(
                     [
                         dbc.Col(
