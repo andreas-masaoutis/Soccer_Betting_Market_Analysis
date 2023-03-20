9bf2212870ca4df200a15e37cc1c29f2860ebb44
andreas-masaoutis <41254497+andreas-masaoutis@users.noreply.github.com>
2023-03-20 05:22:09 +0200

    Updated text and graphs

--- apps/introduction.py
+++ apps/introduction.py
@@ -3,6 +3,8 @@ The introductory page
 """
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+
 
 with open("texts/introduction.md", encoding="utf-8") as md_file:
     md_content = md_file.read()
@@ -10,6 +12,14 @@ with open("texts/introduction.md", encoding="utf-8") as md_file:
 
 # change to app.layout if running as single page app instead
 layout = html.Div(
-    children=[dcc.Markdown(f"""{md_content}""")],
-    style={"margin": "0% 10% ", "text-align": "justify"},
+    [
+        dbc.Container(
+            [
+                html.Div(
+                    [dcc.Markdown(f"""{md_content}""")],
+                    style={"text-align": "justify", "font-size": "17px"},
+                ),
+            ]
+        )
+    ],
 )
